94e4eb84f9d3537beece5e7c751c5ea7c5184315
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2025-04-15 22:24:46 +0300

    Asp.Net Core MultiShop Mikroservis E-Ticaret
    Bölüm 1: Merhaba Mikroservis
    Giriş,
    Tema Dosyası,
    Projede Yer alacak Mikroservisler,
    Mikroservis Nedir
    
    Bölüm 2: Catalog Mikroservisi ve MongoDb
    Projenin Oluşturulması,
    MongoDb Nedir,
    Product ve Category Entityleri,
    ProductImage ve Detail Entityleri,
    MongoDb AppSettingJson Düzenlemeleri,
    Settingslerin Yazılması,
    Product ve Category Dtolar,
    Bazı Düzenlemeler,
    Product Detail ve Image Dtoları,
    Auto Mapper

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/ProductDetailsDtos/GetByIDProductDetailDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiShop.Catalog.Dtos.ProductDetailsDtos
+{
+    public class GetByIDProductDetailDto
+    {
+        public string ProductDetailID { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductInfo { get; set; }
+    }
+}
