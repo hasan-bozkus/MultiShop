ae6157090372f96c1d1362df7f83461b004fb90c
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2025-04-19 21:35:27 +0300

    Asp.Net Core MultiShop Mikroservis E-Ticaret
    Bölüm 5: Onion Architecture & CQRS Mediator Tasarım Desenleri ve Order Mikroservisi
    Onion Mimasirisine Giriş,
    Order Mikrosevisinenin Oluşturulması,
    Order Domain Katamanı,
    CQRS Nedir,
     Address Query ve Result Sınıfları,
    Commandlar ve Create Address Command Handler,
    Addressleri Listeleme ve Id'ye Göre Getirme Handle Metotları,
    Address Remove Update Handler Metotları,
    Repository Sınıfı,
    OrderDetail CQRS İşlemleri,
    Order Detail Handlerların Tamamlanması,
    Address Apileri,
    Order Detail Apilerinin Yazılması

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/GetOrderDeatilByIdQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Queries.OrderDetailQueries;
+using MultiShop.Order.Application.Features.CQRS.Results.OrderDetailResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers
+{
+    public class GetOrderDeatilByIdQueryHandler
+    {
+        private readonly IRepository<OrderDetail> _repository;
+
+        public GetOrderDeatilByIdQueryHandler(IRepository<OrderDetail> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetOrderDetailByIdQueryResult> Handle(GetOrderDetailByIdQuery query)
+        {
+            var value = await _repository.GetByIdAsync(query.Id);
+            return new GetOrderDetailByIdQueryResult
+            {
+                OrderDeatilId = value.OrderDeatilId,
+                ProductId = value.ProductId,
+                ProductName = value.ProductName,
+                ProductPrice = value.ProductPrice,
+                ProductAmount = value.ProductAmount,
+                ProductTotalPrice = value.ProductTotalPrice,
+                OrderingId = value.OrderingId
+            };
+        }
+    }
+}
---
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Controllers/OrderDeatilsController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands;
+using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using MultiShop.Order.Application.Features.CQRS.Queries.OrderDetailQueries;
+
+namespace MultiShop.Order.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderDeatilsController : ControllerBase
+    {
+        private readonly GetOrderDeatilByIdQueryHandler _getOrderDeatilByIdQueryHandler;
+        private readonly CreateOrderDetailCommandHandler _createOrderDetailCommandHandler;
+        private readonly GetOrderDeatilQueryHandler _getOrderDetailQueryHandler;
+        private readonly UpdateOrderDetailCommandHandler _updateOrderDetailCommandHandler;
+        private readonly RemoveOrderDetailCommandHandler _removeOrderDetailCommandHandler;
+
+        public OrderDeatilsController(GetOrderDeatilByIdQueryHandler getOrderDeatilByIdQueryHandler, CreateOrderDetailCommandHandler createOrderDetailCommandHandler, GetOrderDeatilQueryHandler getOrderDetailQueryHandler, UpdateOrderDetailCommandHandler updateOrderDetailCommandHandler, RemoveOrderDetailCommandHandler removeOrderDetailCommandHandler)
+        {
+            _getOrderDeatilByIdQueryHandler = getOrderDeatilByIdQueryHandler;
+            _createOrderDetailCommandHandler = createOrderDetailCommandHandler;
+            _getOrderDetailQueryHandler = getOrderDetailQueryHandler;
+            _updateOrderDetailCommandHandler = updateOrderDetailCommandHandler;
+            _removeOrderDetailCommandHandler = removeOrderDetailCommandHandler;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> OrderDeatilList()
+        {
+            var values = await _getOrderDetailQueryHandler.Handle();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrderDeatil(int id)
+        {
+            var values = await _getOrderDeatilByIdQueryHandler.Handle(new GetOrderDetailByIdQuery(id));
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrderDeatil(CreateOrderDetailCommand command)
+        {
+            await _createOrderDetailCommandHandler.Handle(command);
+            return Ok("Ekleme işlemi başarılı");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateOrderDeatil(UpdateOrderDetailCommand command)
+        {
+            await _updateOrderDetailCommandHandler.Handle(command);
+            return Ok("Güncelleme işlemi başarılı");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrderDetail(int id)
+        {
+            await _removeOrderDetailCommandHandler.Handle(new RemoveOrderDetailCommand(id));
+            return Ok("Silme işlemi başarılı");
+        }
+    }
+}
