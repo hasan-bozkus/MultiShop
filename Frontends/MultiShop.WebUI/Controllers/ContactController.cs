45422117099fd13eb7e59916c775590066a2e2cd
Hasan BOZKUŞ <hasanbozkus0147@outlook.com>
2025-05-19 15:25:48 +0300

    Asp.Net Core MultiShop Mikroservis E-Ticaret
    Bölüm 27: Contact İşlemleri
    Contact Api,
    Mesaj Gönderme İşlemi

--- Frontends/MultiShop.WebUI/Controllers/ContactController.cs
+++ Frontends/MultiShop.WebUI/Controllers/ContactController.cs
@@ -1,12 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ContactDtos;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
 
 namespace MultiShop.WebUI.Controllers
 {
     public class ContactController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateContact(CreateContactDto createContactDto)
+        {
+            createContactDto.IsRead = false;
+            createContactDto.SendDate = DateTime.Parse(DateTime.UtcNow.ToString());
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createContactDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44320/api/Contacts", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return NoContent();
         }
     }
 }
---
+++ Services/Catalog/MultiShop.Catalog/Controllers/ContactsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ContactDtos;
+using MultiShop.Catalog.Services.ContactServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IContactServices _contactServices;
+
+        public ContactsController(IContactServices contactServices)
+        {
+            _contactServices = contactServices;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ContactList()
+        {
+            var categories = await _contactServices.GetAllContactAsync();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetContactByID(string id)
+        {
+            var values = await _contactServices.GetByIDContactAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateContact(CreateContactDto createContactDto)
+        {
+            await _contactServices.CreateContactAsync(createContactDto);
+            return Ok("Ekleme işlemi başarılı");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteContact(string id)
+        {
+            await _contactServices.DeleteContactAsync(id);
+            return Ok("Silme işlemi başarılı");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateContact(UpdateContactDto updateContactDto)
+        {
+            await _contactServices.UpdateContactAsync(updateContactDto);
+            return Ok("Güncelleme işlemi başarılı");
+        }
+    }
+}
