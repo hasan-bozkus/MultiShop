6f48d9cf9ff3e9302daa2988c739bbfabf359632
Hasan BOZKUŞ <hasanbozkus0147@outlook.com>
2025-05-25 13:57:07 +0300

    Asp.Net Core MultiShop Mikroservis E-Ticaret
    Bölüm 33: Service Api Settings
    Service Api Settings App Setting Json Konfigürasyonu,
    Service Api Settings Sınıfı,
    Service Api Settings Konfigürasyonu ve Identity Service Url Yapılandırması,
    Identity Service'de Break Point,
    Config'de Manager Profile Bilgileri,
    Refresh Token

--- Frontends/MultiShop.WebUI/Program.cs
+++ Frontends/MultiShop.WebUI/Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddScoped<ILoginService, LoginService>();
 builder.Services.AddHttpClient<IIdentityService, IdentityService>();
 
 builder.Services.Configure<ClientSettings>(builder.Configuration.GetSection("ClientSettings"));
+builder.Services.Configure<ServiceApiSettings>(builder.Configuration.GetSection("ServiceApiSettings"));
 
 builder.Services.AddHttpClient();
 builder.Services.AddControllersWithViews();
--- IdentityServer/MultiShop.IdentityServer/Config.cs
+++ IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -66,7 +66,11 @@ namespace MultiShop.IdentityServer
                 ClientName = "Multi Shop Manager User",
                 AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
                 ClientSecrets = {new Secret("multishopsecret".Sha256())},
-                AllowedScopes = { "CatalogReadPermission", "CatalogFullPermission", "BasketFullPermission", "OcelotFullPermission", "CommentFullPermission", "PaymentFullPermission" }
+                AllowedScopes = { "CatalogReadPermission", "CatalogFullPermission", "BasketFullPermission", "OcelotFullPermission", "CommentFullPermission", "PaymentFullPermission",
+                IdentityServerConstants.LocalApi.ScopeName,
+                IdentityServerConstants.StandardScopes.Email,
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile}
             },
 
             //Admin
