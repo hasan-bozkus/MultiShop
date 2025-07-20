9df7008dadd187d6c441fb78578e65897bf49c3f
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2025-07-20 12:35:57 +0300

    Asp.Net Core MultiShop Mikroservis E-Ticaret
    Bölüm 51: RabbitMQ
    RabbitMQ Nedir,
    RabbitMQ Çalışma Prensibi,
    RabbitMQ Bir Makale Daha,
    Erlang ve RabbitMQ Kurulumları,
    RabbitMQ Kurulumu,
    RabbitMQ 15672 Port Ekranı,
    RabmbitMQ İçin Proje Oluşturma,
    RabbitMQ Client Paketi ve Mesaj Gönderme Kodları,
    Mesaj Gönderme İşlemi,
    Posta Kutusu Örneği,
    Mesaj Okuma İşlemi,
    Mesaj Conusme İşleminin Tamamlanması

---
+++ Services/RabbitMQMessage/MultiShop.RabbitMQMessage/Controllers/MessagesController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace MultiShop.RabbitMQMessage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateMessage()
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "******************************"
+            };
+
+            var connection = connectionFactory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("Kuyruk2", false, false,false, arguments: null);
+
+            var messageContent = "Bugün Hava Çok Soğuk.";
+
+            var byteMessageContent = Encoding.UTF8.GetBytes(messageContent);
+
+            channel.BasicPublish(exchange: "", routingKey:"Kuyruk1", basicProperties: null, body: byteMessageContent);
+
+            return Ok("Mesajınız Kuyruğa Alınmıştır.");
+        }
+
+        private static string message;
+
+        [HttpGet]
+        public IActionResult GetMessage()
+        {
+
+            var factory = new ConnectionFactory();
+
+            factory.HostName = "localhost";
+            factory.UserName = "guest";
+            factory.Password = "***************************";
+            
+
+            var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, body) =>
+            {
+                var byteMessage = body.Body.ToArray();
+                message = Encoding.UTF8.GetString(byteMessage);
+            };
+
+            channel.BasicConsume(queue: "Kuyruk1", autoAck: false, consumer: consumer);
+
+            if (string.IsNullOrEmpty(message))
+            {
+                return NoContent();
+            }
+            else
+            {
+                return Ok(message);
+            }
+        }
+    }
+}
