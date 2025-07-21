04e946bcc8e056d3c7357851b2134e9d013f2ffb
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2025-07-21 10:56:33 +0300

    Asp.Net Core MultiShop Mikroservis E-Ticaret
    Bölüm 52: Rapid Api
    Rapid Api Giriş,
    Rapid Api Kodlarının Alınması,
    Hava Durumu Bilgisinin Getirilmesi,
    Dolar ve Euro Kur Bilgilerini Api Üzerinden Çekme,
    E-Commerce Api List Hazırlığı,
    E-Commerce Listenisini Getirme,
    Bir Küçük Case

---
+++ RapidApi/MultiShop.RapidApiWebUI/Controllers/DefaultController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.RapidApiWebUI.Models;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace MultiShop.RapidApiWebUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public async Task<IActionResult> WeatherDeatil()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://the-weather-api.p.rapidapi.com/api/weather/mardin"),
+                Headers =
+    {
+        { "x-rapidapi-key", "0d8cb4f5b0mshcf6e94f4f120a03p1e6e5bjsn85b6be86fe40" },
+        { "x-rapidapi-host", "the-weather-api.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<WeatherViewModel>(body);
+                ViewBag.cityTemp = values.data.temp;
+                return View();
+            }
+        }
+
+        public async Task<IActionResult> Exchange()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://real-time-finance-data.p.rapidapi.com/currency-exchange-rate?from_symbol=USD&to_symbol=TRY&language=en"),
+                Headers =
+    {
+        { "x-rapidapi-key", "0d8cb4f5b0mshcf6e94f4f120a03p1e6e5bjsn85b6be86fe40" },
+        { "x-rapidapi-host", "real-time-finance-data.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel>(body);
+                ViewBag.usdToTRY = values.data.exchange_rate;
+                ViewBag.tryToUsd = values.data.previous_close;
+            }    
+            
+            var request2 = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://real-time-finance-data.p.rapidapi.com/currency-exchange-rate?from_symbol=EUR&to_symbol=TRY&language=en"),
+                Headers =
+    {
+        { "x-rapidapi-key", "0d8cb4f5b0mshcf6e94f4f120a03p1e6e5bjsn85b6be86fe40" },
+        { "x-rapidapi-host", "real-time-finance-data.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request2))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel>(body);
+                ViewBag.eurToTRY = values.data.exchange_rate;
+                ViewBag.tryToEur = values.data.previous_close;
+            }      
+            
+            
+            return View();
+
+        }
+    }
+}
---
+++ RapidApi/MultiShop.RapidApiWebUI/Models/ECommerceViewModel.cs
@@ -0,0 +1,175 @@
+﻿namespace MultiShop.RapidApiWebUI.Models
+{
+    public class ECommerceViewModel
+    {
+        public string status { get; set; }
+        public string request_id { get; set; }
+        public Data data { get; set; }
+
+
+        public class Data
+        {
+            public Product[] products { get; set; }
+            public object[] sponsored_products { get; set; }
+            public object[] filters { get; set; }
+            public string q { get; set; }
+        }
+
+        public class Product
+        {
+            public string product_id { get; set; }
+            public string product_title { get; set; }
+            public string product_description { get; set; }
+            public string[] product_photos { get; set; }
+            public Product_Videos[] product_videos { get; set; }
+            public Product_Attributes product_attributes { get; set; }
+            public float product_rating { get; set; }
+            public string product_page_url { get; set; }
+            public int product_num_reviews { get; set; }
+            public int product_num_offers { get; set; }
+            public object typical_price_range { get; set; }
+            public Current_Product_Variant_Properties current_product_variant_properties { get; set; }
+            public Product_Variants product_variants { get; set; }
+            public Reviews_Insights reviews_insights { get; set; }
+            public Offer offer { get; set; }
+        }
+
+        public class Product_Attributes
+        {
+            public string DPI { get; set; }
+            public string Sensor { get; set; }
+            public string NumberofButtons { get; set; }
+            public string PollingRate { get; set; }
+            public string TrackingSpeed { get; set; }
+            public string Programmable { get; set; }
+            public string ResponseTime { get; set; }
+            public string OS { get; set; }
+            public string Devices { get; set; }
+            public string MouseType { get; set; }
+            public string ConnType { get; set; }
+            public string CableLength { get; set; }
+            public string USBType { get; set; }
+            public string HandOrientation { get; set; }
+            public string GripStyle { get; set; }
+            public string Adjustability { get; set; }
+            public string ErgonomicDesign { get; set; }
+            public string Vertical { get; set; }
+            public string WristRest { get; set; }
+            public string Dimensions { get; set; }
+            public string Shape { get; set; }
+            public string Weight { get; set; }
+            public string Material { get; set; }
+            public string Finish { get; set; }
+            public string ScrollWheelType { get; set; }
+            public string Style { get; set; }
+            public string Lighting { get; set; }
+            public string Mini { get; set; }
+            public string Multitouch { get; set; }
+            public string Highlights { get; set; }
+            public string Software { get; set; }
+            public string TopUseCases { get; set; }
+            public string TypicalUsers { get; set; }
+            public string WirelessTech { get; set; }
+            public string ReceiverType { get; set; }
+            public string PalmSupport { get; set; }
+            public string SensorType { get; set; }
+            public string Accuracy { get; set; }
+            public string Resolution { get; set; }
+            public string TrackingTech { get; set; }
+            public string LiftOffDistance { get; set; }
+            public string UseCases { get; set; }
+            public string Programmability { get; set; }
+            public string ButtonCount { get; set; }
+            public string Customization { get; set; }
+            public string MacroKeys { get; set; }
+            public string ButtonLocation { get; set; }
+            public string Ergonomic { get; set; }
+            public string Depth { get; set; }
+            public string Height { get; set; }
+            public string Width { get; set; }
+            public string OSCompat { get; set; }
+            public string DeviceCompat { get; set; }
+            public string SoftwareCompat { get; set; }
+            public string AccessoryType { get; set; }
+            public string MouseampKeyboard { get; set; }
+            public string Wired { get; set; }
+            public string Wireless { get; set; }
+            public string Bluetooth { get; set; }
+            public string USBReceiver { get; set; }
+            public string DongleType { get; set; }
+            public string DongleStorage { get; set; }
+            public string Type { get; set; }
+            public string Subtype { get; set; }
+            public string Gaming { get; set; }
+            public string HandSize { get; set; }
+            public string ProfUse { get; set; }
+            public string SkillLevel { get; set; }
+            public string AgeRange { get; set; }
+            public string Ambidextrous { get; set; }
+            public string MultiTouch { get; set; }
+            public string RGBLighting { get; set; }
+            public string SpecialFeatures { get; set; }
+            public string WirelessCharge { get; set; }
+            public string CustomizationOptions { get; set; }
+            public string DriverUpdates { get; set; }
+            public string ProfileSupport { get; set; }
+            public string ReleaseDate { get; set; }
+            public string Brand { get; set; }
+            public string LeftHanded { get; set; }
+            public string Colour { get; set; }
+            public string Connectivity { get; set; }
+            public string Sensitivity { get; set; }
+        }
+
+        public class Current_Product_Variant_Properties
+        {
+            public string Colour { get; set; }
+        }
+
+        public class Product_Variants
+        {
+            public Colour[] Colour { get; set; }
+        }
+
+        public class Colour
+        {
+            public string name { get; set; }
+            public string thumbnail { get; set; }
+            public string product_id { get; set; }
+        }
+
+        public class Reviews_Insights
+        {
+        }
+
+        public class Offer
+        {
+            public string offer_id { get; set; }
+            public string offer_title { get; set; }
+            public string offer_page_url { get; set; }
+            public string price { get; set; }
+            public string shipping { get; set; }
+            public bool on_sale { get; set; }
+            public object original_price { get; set; }
+            public string product_condition { get; set; }
+            public string store_name { get; set; }
+            public string store_rating { get; set; }
+            public int store_review_count { get; set; }
+            public string store_reviews_page_url { get; set; }
+            public string store_favicon { get; set; }
+            public object payment_methods { get; set; }
+        }
+
+        public class Product_Videos
+        {
+            public string title { get; set; }
+            public string url { get; set; }
+            public string source { get; set; }
+            public string publisher { get; set; }
+            public string thumbnail { get; set; }
+            public string preview_url { get; set; }
+            public int duration_ms { get; set; }
+        }
+
+    }
+}
