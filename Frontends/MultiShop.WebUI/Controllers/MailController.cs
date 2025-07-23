a068a026a4d98e1e6d6599a796444eada7b40369
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2025-07-23 16:38:29 +0300

    Asp.Net Core MultiShop Mikroservis E-Ticaret
    Bölüm 54: Mail Gönderme İşlemleri
    Mail Uygulama Şifresi Alma,
    Mail Gönderme Backend Kodları,
    Mail Gönderme İşlemi
    
    Bölüm 55: Öğrencilerden Gelen Kod Örnekleri
    Öğrencilerden Gelen Soru ve Cevaplar
    
    Bölüm 56: Final
    Son Söz,
    Github ve Linkedin Paşlaşımları,
    Final
    
    Bölüm 57: Kaynak Dosyalar
    Proje Kaynak Dosyası,
    Kullanıcı Tema Dosyası

---
+++ Frontends/MultiShop.WebUI/Controllers/MailController.cs
@@ -0,0 +1,39 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MultiShop.WebUI.Models;
+namespace MultiShop.WebUI.Controllers
+{
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult SendMail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMail(MailRequest mailRequest)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("MultiShop Katalog", "denememardin0147@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", mailRequest.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            mimeMessage.Subject = mailRequest.Subject;
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.HtmlBody = mailRequest.MessageContent;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate("denememardin0147@gmail.com", "apmetyokiyxtdvwm");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+            return View();
+        }
+    }
+}
